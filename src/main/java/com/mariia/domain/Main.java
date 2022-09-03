d76599e3d257e35e90ed36c0014a2347d31c7490
MargoSamo <margo_ii@mail.ru>
2022-09-03 17:55:34 +0400

    input console

--- src/main/java/com/mariia/domain/Main.java
+++ src/main/java/com/mariia/domain/Main.java
@@ -1,5 +1,6 @@
 package com.mariia.domain;
 
+import net.minidev.json.JSONUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -8,6 +9,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
@@ -15,19 +17,49 @@ public class Main {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         try (SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-             session.beginTransaction();
+            session.beginTransaction();
 
 //             Chief load = session.load(Chief.class, 1);
 //             Employee get = session.get(Employee.class, 1);
 //            System.out.println(get.getName());
 //            System.out.println(load.getName());
 
-            Query query = session.createQuery("from Worktime where the_date='2022-08-01'");
+           /* Query query = session.createQuery("from Worktime where the_date='2022-08-01'");
             List<Worktime> list = query.list();
             System.out.println(list);
-
-
+*/
             session.getTransaction().commit();
+        }
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter command:\n" +
+                "c - create new employee\n" +
+                "g - get employee entrance date list\n" +
+                "a - add entrance date for a specific employee\n" +
+                "q - exit");
+
+        if (console.hasNext()) {
+            String userInput = console.next();
+
+            switch (userInput) {
+                case "c":
+                    System.out.println(1);
+                    break;
+                case "g":
+                    System.out.println(2);
+                    break;
+                case "a":
+                    System.out.println(3);
+                    break;
+                case "q":
+                    System.out.println(4);
+                    break;
+                default:
+                    System.out.println("don't correct data");
+                    break;
+            }
+
         }
     }
 }
